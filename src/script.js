9fbeaa1cdcd55c41f9a23301116e0e8bebe06e63
Yo Yehudi <yochannah@gmail.com>
2018-06-30 14:32:30 -0700

    add redirect button

--- src/script.js
+++ src/script.js
@@ -1,28 +1,50 @@
+//useful for debug until I have a real redirect coming in to read from.
+var fakeParams = {
+  "identifierType" : "Gene",
+  "identifiers" : ["pparg", "GATA1", "AVP"],
+  "organism" : "H. sapiens"
+}
+
+//let's make the list of mines globally accessible
+var mines;
+
+//get all InterMines as soon as the DOM has loaded
 document.addEventListener("DOMContentLoaded", function() {
-  //get all interMines
   $.ajax("http://registry.intermine.org/service/instances").then(function(response) {
-    var mines = response.instances;
+    //storing mines globally
+    mines = response.instances;
+
     var minesList = document.getElementById("interMinesList");
 
     //debug. remove when done.
     console.log(mines);
 
-    //create a new mine dom node
-    //and add it to the list of mines
+    //once everything is loaded, display a list of mines to the user
     mines.map(function(mine) {
       minesList.append(mineNode(mine));
     });
 
   });
-
-  function mineNode(mine) {
-    //Generate text for links to each mine.
-    var mineNode = document.createElement("tr"),
-      mineRow = "<td><a href='" + mine.url + "'>" + mine.name + "</td>" +
-      "<td>" + mine.organisms.join(", ") + "</td>";
-    mineNode.organisms = mine.organisms;
-    mineNode.setAttribute("class", "mineEntry");
-    mineNode.innerHTML = mineRow;
-    return mineNode;
-  }
 });
+
+function mineNode(mine) {
+  //Generate text for links to each mine.
+  var mineNode = document.createElement("tr"),
+    mineRow = "<td>" + mine.name + "</td>" +
+    "<td>" + mine.organisms.join(", ")
+    + "</td><td class='exportToMine'>" + mineNav(mine.url, fakeParams) + "</td>";
+  mineNode.organisms = mine.organisms;
+  mineNode.setAttribute("class", "mineEntry");
+  mineNode.innerHTML = mineRow;
+  return mineNode;
+}
+
+function mineNav(mine, data) {
+return '<form action="' + mine +'/portal.do" name="list" method="post">' +
+         '<input type="hidden" name="externalids" value="' +
+                  fakeParams.identifiers.join(",") + '" />' +
+         '<input type="hidden" name="class" value="' +
+                  fakeParams.identifierType  + '" />' +
+         '<input type="submit" value="Submit" />' +
+       '</form>'
+}
